3d12ff07dcda976241f1ead29bf16d095edae33e
Tomasz Mamala <tomaszmamala@gmail.com>
2023-06-26 20:28:46 +0200

    Added adding questions and reading questions in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const CORS = require('cors');
 
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = 5000;
@@ -19,7 +19,7 @@ app.use(
   })
 );
 
-app.use(express.json()); 
+app.use(express.json());
 
 const pgp = require('pg-promise')();
 const db = pgp(process.env.DATABASE_URL);
@@ -37,7 +37,7 @@ app.get('/quizzes', async (_: any, res: any) => {
 app.get('/quizzes/:id', async (req: any, res: any) => {
   try {
     const quiz = await db.one('SELECT * FROM quizzes WHERE id = $1', [
-      req.params.id,
+      req.params.id as Number,
     ]);
     res.json(quiz);
   } catch (error) {
@@ -46,7 +46,7 @@ app.get('/quizzes/:id', async (req: any, res: any) => {
   }
 });
 
-app.post('/addquiz', async (req, res) => {
+app.post('/addquiz', async (req: any, res: any) => {
   try {
     const { name, description } = req.body;
 
@@ -56,10 +56,84 @@ app.post('/addquiz', async (req, res) => {
 
     const quiz = await db.one(
       'INSERT INTO quizzes (name, description) VALUES ($1, $2) RETURNING *',
-      [name, description]
+      [name as String, description as String]
     );
 
     res.json(quiz);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Wystąpił błąd serwera.' });
+  }
+});
+
+app.post('/addquestions', async (req: any, res: any) => {
+  try {
+    if (
+      !req.body ||
+      !req.body.questions.every(
+        (question: any) =>
+          question.quizId &&
+          question.question &&
+          question.question_type &&
+          question.answers.every(
+            (answer: any) => answer.answer && answer.isCorrect !== undefined
+          )
+      )
+    ) {
+      return res.sendStatus(400);
+    }
+
+    for (const data of req.body.questions) {
+      const { quizId, question, question_type, answers } = data;
+
+      if (!quizId || !question || !question_type) {
+        return res.status(400).json({ error: 'Nieprawidłowe dane wejściowe.' });
+      }
+
+      const lastQuestion = await db.one(
+        'INSERT INTO quizquestions (quiz_id, question, question_type) VALUES ($1, $2, $3) RETURNING id',
+        [quizId, question, question_type]
+      );
+
+      for (const questionAnswer of answers) {
+        const { answer, isCorrect } = questionAnswer;
+
+        if (!answer || isCorrect === undefined) {
+          return res
+            .status(400)
+            .json({ error: 'Nieprawidłowe dane wejściowe.' });
+        }
+
+        await db.one(
+          'INSERT INTO questionanswers (questionid, answer, iscorrect) VALUES ($1, $2, $3) RETURNING *',
+          [lastQuestion.id, answer, `${+isCorrect}`]
+        );
+      }
+    }
+
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Wystąpił błąd serwera.' });
+  }
+});
+
+app.get('/question/:id', async (req, res) => {
+  try {
+    const questionId = req.params.id;
+
+    const question = await db.one('SELECT * FROM quizquestions WHERE id = $1', [
+      questionId,
+    ]);
+
+    const answers = await db.any(
+      'SELECT * FROM questionanswers WHERE questionid = $1',
+      [questionId]
+    );
+
+    question.answers = answers;
+
+    res.json(question);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Wystąpił błąd serwera.' });
